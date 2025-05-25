634f3f96a6af13fd0e27a4354dc63fd48952b276
Marcin Kralka <marcink96@gmail.com>
2025-05-25 22:35:13 +0200

    Okay. Now let's actually let user choice where he wants to put files.

--- build/upgrade-dotfiles.sh
+++ build/upgrade-dotfiles.sh
@@ -5,9 +5,9 @@ root_dir=$current_dir/..
 
 os_target=$(cat $root_dir/.dotfiles_os_target)
 
-mv $root_dir/.* $HOME
+mv $root_dir/.* .
 
-bin_dir=$HOME/bin
+bin_dir=./bin
 
 if [ ! -d $bin_dir ]; then
     mkdir $bin_dir
