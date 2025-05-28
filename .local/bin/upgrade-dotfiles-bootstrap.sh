8ac79289c4596fa0def14700fef8c779ebaceca0
Marcin Kralka <marcink96@gmail.com>
2025-05-28 23:18:41 +0200

    Downgraded upgrade-dotfiles-bootstrap.sh to a Bourne Shell file. It doesn't need Bash to execute.

--- .local/bin/upgrade-dotfiles-bootstrap.sh
+++ .local/bin/upgrade-dotfiles-bootstrap.sh
@@ -1,22 +1,22 @@
-#!/usr/bin/env bash
+#!/bin/sh
 
 github_base_url='https://github.com/Matriks404/dotfiles'
 
 # Download and unzip archive containing dotfiles and scripts.
 dotfiles_dir='dotfiles-master'
 
-echo -e "=== Getting dotfiles zip file... ==="
+echo "=== Getting dotfiles zip file... ==="
 wget "$github_base_url/archive/refs/heads/master.zip"
 
-echo -e "=== Unzipping dotfiles... ==="
+echo "=== Unzipping dotfiles... ==="
 unzip master.zip -x "$dotfiles_dir/README.md" "$dotfiles_dir/.gitignore" "$dotfiles_dir/tools/hooks/*" "$dotfiles_dir/tools/*"
 
 # Boostrap the script that upgrades dotfiles.
-echo -e "=== Bootstrapping 'build/update-dotfiles.sh Bash script... ==="
+echo "=== Bootstrapping 'build/update-dotfiles.sh Bash script... ==="
 $dotfiles_dir/build/upgrade-dotfiles.sh
 
 # Cleanup.
-echo -e "=== Doing some final cleanup... ===
+echo "=== Doing some final cleanup... ===
 rm "$dotfiles_dir/build/upgrade-dotfiles.sh"
 rmdir -p "$dotfiles_dir/build"
 
