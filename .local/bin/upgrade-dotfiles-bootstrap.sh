a4c5be5403d097632099a7b60871d5f0ebca58dd
Marcin Kralka <marcink96@gmail.com>
2025-05-28 22:59:32 +0200

    Try fixing not being able to exclude tools directory when unzipping dotfiles, by having quotes on the paths of excluded files.

--- .local/bin/upgrade-dotfiles-bootstrap.sh
+++ .local/bin/upgrade-dotfiles-bootstrap.sh
@@ -1,17 +1,17 @@
 #!/usr/bin/env bash
 
-github_base_url=https://github.com/Matriks404/dotfiles
+github_base_url='https://github.com/Matriks404/dotfiles'
 
 # Download and unzip archive containing dotfiles and scripts.
-dotfiles_dir=dotfiles-master
-wget $github_base_url/archive/refs/heads/master.zip
-unzip master.zip -x $dotfiles_dir/README.md $dotfiles_dir/.gitignore $dotfiles_dir/tools/*
+dotfiles_dir='dotfiles-master'
+wget "$github_base_url/archive/refs/heads/master.zip"
+unzip master.zip -x "$dotfiles_dir/README.md $dotfiles_dir/.gitignore $dotfiles_dir/tools/*"
 
 # Boostrap the script that upgrades dotfiles.
 $dotfiles_dir/build/upgrade-dotfiles.sh
 
 # Cleanup.
-rm $dotfiles_dir/build/upgrade-dotfiles.sh
-rmdir -p $dotfiles_dir/build
+rm "$dotfiles_dir/build/upgrade-dotfiles.sh"
+rmdir -p "$dotfiles_dir/build"
 
-rm master.zip
+rm "master.zip"
