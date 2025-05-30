588dbf3af82dbcc763e92ce3eab63f999faffee4
Marcin Kralka <marcink96@gmail.com>
2025-05-30 18:48:07 +0200

    Oops (not sure what added these files).

--- dotfiles-master/tools/initial_repository_setup.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env bash
-
-# Configure git hooks directory to 'build/hooks'
-
-git config --local core.hooksPath ./tools/hooks
-chmod +x ./tools/hooks/pre-commit
-echo "Git hooks configured!"
