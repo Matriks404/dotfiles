eccdeea84b7bda6757b883ed2cd721510d00e51c
Marcin Kralka <marcink96@gmail.com>
2025-05-30 18:29:25 +0200

    Nearly done, only need to clean up files properly.

--- build/update.sh
+++ build/update.sh
@@ -12,21 +12,25 @@ echo "=== Unzipping dotfiles... ==="
 unzip master.zip -x "$dotfiles_dir/README.md" "$dotfiles_dir/.gitignore" "$dotfiles_dir/build/*" "$dotfiles_dir/tools/hooks/*" "$dotfiles_dir/tools/*"
 
 echo "=== Moving dotfiles... ==="
-dotfiles_to_move="$(cat $dotfiles_dir/.dotfiles_lists/common.txt)"
+dotfiles_to_move_list="$(cat $dotfiles_dir/.dotfiles_lists/common.txt)"
 
 if [ "$USER" = "marcin" ]; then
     full_username=$(getent passwd marcin | cut -d ':' -f 5)
 
     if echo "$full_username" | grep -q "^Marcin Kralka"; then
-        #dotfiles_to_copy="$dotfiles_to_copy $dotfiles_dir/.gitconfig"
-        dotfiles_to_move="$dotfiles_to_move $(cat $dotfiles_dir/.dotfiles_lists/private.txt)"
+        dotfiles_to_move_list="$dotfiles_to_move_list $(cat $dotfiles_dir/.dotfiles_lists/private.txt)"
 
         if [ -f /etc/debian_version ]; then
-            #dotfiles_to_copy="$dotfiles_to_copy $dotfiles_dir/.reportbugrc"
-            dotfiles_to_move="$dotfiles_to_move $(cat $dotfiles_dir/.dotfiles_lists/private_debian.txt)"
+            dotfiles_to_move_list="$dotfiles_to_move_list $(cat $dotfiles_dir/.dotfiles_lists/private_debian.txt)"
         fi
     fi
 fi
+
+dotfiles_to_move=""
+
+for filename in $dotfiles_to_move_list; do
+    dotfiles_to_move="$dotfiles_to_move $dotfiles_dir/$filename"
+done
 
 mv -v $dotfiles_to_move .
 
@@ -37,26 +41,33 @@ mkdir -p $bin_dir
 mv -v $dotfiles_dir/.local/bin/* $bin_dir
 
 echo "=== Moving dotfiles lists... ==="
+
+txtfiles_dir="./.dotfiles_lists"
 txtfiles_to_move="$dotfiles_dir/.dotfiles_lists/*"
 
-mv -v $txtfiles_to_move .
+mkdir -p $txtfiles_dir
+mv -v $txtfiles_to_move $txtfiles_dir
 
 # Cleanup
 
 echo "=== Cleaning up... ==="
 
 if [ -f $dotfiles_dir/.gitconfig ]; then
-    rm $dotfiles_dir/.gitconfig
+    rm -v $dotfiles_dir/.gitconfig
 fi
 
 if [ -f $dotfiles_dir/.reportbugrc ]; then
-    rm $dotfiles_dir/.reportbugrc
+    rm -v $dotfiles_dir/.reportbugrc
 fi
 
-rmdir $dotfiles_dir/.local/bin
-rmdir $dotfiles_dir/.local
-rmdir $dotfiles_dir
+rm -v $dotfiles_dir/.dotfiles_os_target
 
-rm master.zip
+rmdir -v $dotfiles_dir/.dotfiles_lists
+
+rmdir -v $dotfiles_dir/.local/bin
+rmdir -v $dotfiles_dir/.local
+rmdir -v $dotfiles_dir
+
+rm -v master.zip
 
 echo "\nEverything is done! Make sure to restart your Bash instance to get all new features and improvements!"
