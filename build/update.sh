1b0a5437ee42a856ae4e29e7bec9b68b075eea3f
Marcin Kralka <marcink96@gmail.com>
2025-05-30 17:50:39 +0200

    Part 2 of simplying specifiying dotfiles to copy/move.

--- build/update.sh
+++ build/update.sh
@@ -11,28 +11,36 @@ wget "$github_base_url/archive/refs/heads/master.zip"
 echo "=== Unzipping dotfiles... ==="
 unzip master.zip -x "$dotfiles_dir/README.md" "$dotfiles_dir/.gitignore" "$dotfiles_dir/build/*" "$dotfiles_dir/tools/hooks/*" "$dotfiles_dir/tools/*"
 
-dotfiles_to_copy="$dotfiles_dir/.bash* $dotfiles_dir/.dotfiles*"
+#dotfiles_to_copy="$dotfiles_dir/.bash* $dotfiles_dir/.dotfiles*"
+dotfiles_to_move="$(cat $dotfiles_dir/.dotfiles_list_common.txt)"
+
+echo "=== Moving dotfiles lists... ==="
+txtfiles_to_move="$dotfiles_dir/.dotfiles_list_*"
+
+mv -v $txtfiles_to_move .
 
 echo "=== Moving dotfiles... ==="
 if [ "$USER" = "marcin" ]; then
     full_username=$(getent passwd marcin | cut -d ':' -f 5)
 
     if echo "$full_username" | grep -q "^Marcin Kralka"; then
-        dotfiles_to_copy="$dotfiles_to_copy $dotfiles_dir/.gitconfig"
+        #dotfiles_to_copy="$dotfiles_to_copy $dotfiles_dir/.gitconfig"
+        dotfiles_to_move="$dotfiles_to_move $(cat $dotfiles_dir/.dotfiles_list_private.txt)"
 
         if [ -f /etc/debian_version ]; then
-            dotfiles_to_copy="$dotfiles_to_copy $dotfiles_dir/.reportbugrc"
+            #dotfiles_to_copy="$dotfiles_to_copy $dotfiles_dir/.reportbugrc"
+            dotfiles_to_move="$dotfiles_to_move $(cat $dotfiles_dir/.dotfiles_list_private_debian.txt")
         fi
     fi
 fi
 
-mv $dotfiles_to_copy .
+mv -v $dotfiles_to_copy .
 
 echo "=== Moving Bash scripts... ==="
 
 bin_dir="./.local/bin"
 mkdir -p $bin_dir
-mv $dotfiles_dir/.local/bin/* $bin_dir
+mv -v $dotfiles_dir/.local/bin/* $bin_dir
 
 # Cleanup
 
