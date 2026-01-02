b3f00def8d20eae92e6e88e080b22d90d68c6072
Marcin Kralka <marcink96@gmail.com>
2026-01-02 03:45:46 +0100

    Updated backup job script to not be Debian-only.

--- .local/scripts.noexecute/backup_job.sh
+++ .local/scripts.noexecute/backup_job.sh
@@ -1,57 +1,69 @@
 #!/bin/env bash
-#printf -v date '%(%Y-%m-%d)T' -1
+#printf -v date "%(%Y-%m-%d)T" -1
 
 #if sudo -v; then
-    echo -e 'Welcome to Debian 12+ backup program.\n'
+    echo -e "Welcome to Matriks404's backup program."
+    echo -e "Originally written for Debian 12+.\n"
+    echo -e "Version 0.9 (Preview) -- USE ON YOUR OWN RISK.\n\n"
 #else
-#    echo -e 'Authentication failed. Please enter the correct sudo passowrd.'
+#    echo -e "Authentication failed. Please enter the correct sudo passowrd."
 #    exit 1
 #fi
 
-echo -e '* Processing settings shell script ...'
+echo -e "* Processing settings shell script ..."
 source /usr/local/scripts/backup_job_files/settings.sh
 target=$ESSENTIAL_INFO_LOCAL_DIR
 
 if [ -d $target ]; then
     if [ -d $target.old ]; then
-        echo -e 'Removing older version of local backup directory: '$target'.old ...'
+        echo -e "* Removing older version of local backup directory: "$target".old ..."
         rm -r $target.old
     fi
 
-    echo -e 'Moving old local backup directory to: '$target'.old ...'
+    echo -e "* Moving old local backup directory to: "$target".old ..."
     mv $target $target.old
 fi
 
-echo -e '===== backing up apt data ====='
-echo -e 'Backing up apt data to: '$target
+if [ -f "/etc/debian_version" ]; then
+    MAJOR_VERSION=$(cut -d. -f1 /etc/debian_version)
 
-mkdir $target
+    echo -e "\n===== backing up apt data ====="
+    echo -e "Backing up apt data to: "$target
 
-echo -e '* Backing up apt package list ...'
-sudo dpkg --get-selections > $target/apt_package.list
+    mkdir $target
 
-echo -e '* Backing up apt source lists ...'
-mkdir $target/apt_sources/
-sudo cp -R /etc/apt/sources.list* $target/apt_sources/
+    echo -e "* Backing up apt package list ..."
+    sudo dpkg --get-selections > $target/apt_package.list
 
-echo -e '* Backing up apt repository keys ...'
-sudo apt-key exportall > $target/apt_repository.keys
+    echo -e "* Backing up apt source lists ..."
+    mkdir $target/apt_sources/
+    sudo cp -rv /etc/apt/sources.list* $target/apt_sources/
 
-echo -e '===== backing up flatpak data ====='
-echo -e 'Backing up flatpak data to: '$target
+    echo -e "* Backing up apt repository keys ..."
+    if [ "$MAJOR_VERSION" -le 12 ]; then
+        sudo apt-key exportall > $target/apt_repository.keys
+    else
+        sudo cp -rv /etc/apt/keyrings $target/apt_keyrings
+    fi
+fi
 
-echo -e '* Backing up installed flatpak app list ...'
-flatpak list --columns=application --app > $target/flatpaks.txt
+if [ $(command -v flatpak) ]; then
+    echo -e "\n===== backing up flatpak data ====="
+    echo -e "Backing up flatpak data to: "$target
 
-echo -e '===== BACKING UP FILES ====='
+    echo -e "* Backing up installed flatpak app list ..."
+    flatpak list --columns=application --app > $target/flatpaks.txt
+fi
 
-echo -e '* Reading include text file ...'
+echo -e "\n===== BACKING UP FILES ====="
+
+echo -e "* Reading include text file ..."
 INCLUDE_FROM=()
 
 mapfile -t INCLUDE_FROM < /usr/local/scripts/backup_job_files/include.txt
 
 EXCLUDE_FROM=/usr/local/scripts/backup_job_files/exclude.txt
 
-echo -e '* Backing up files to remote host: '$REMOTE_HOSTNAME' in directory: '$REMOTE_LOCATION' ...'
+echo -e "* Backing up files to remote host: "$REMOTE_HOSTNAME" in directory: "$REMOTE_LOCATION" ..."
 rsync -aRv -e "ssh -p $REMOTE_PORT" --delete --exclude-from="$EXCLUDE_FROM" \
 "${INCLUDE_FROM[@]}" "$REMOTE_HOSTNAME:$REMOTE_LOCATION"
